9b1da81791226729768e02b642344a65e9d71719
teamdebuggies <teamdebuggies@gmail.com>
2025-05-18 09:57:59 -0500

    Add terraform/main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "allow_tls" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_exec_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "transaction-logs"
+  acl    = "private"
+}
+
+resource "aws_cloudwatch_log_group" "transaction_logs" {
+  name              = "/aws/lambda/transaction-logs"
+  retention_in_days = 14
+}
+
+// More resources can be added as per requirements
